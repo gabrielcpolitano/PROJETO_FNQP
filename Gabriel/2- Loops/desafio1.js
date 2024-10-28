0535339cd918a90cd85b15135c5e7618ae332478
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-28 16:04:59 -0300

    Thanks God - Desafio 1

--- Gabriel/2- Loops/desafio1.js
+++ Gabriel/2- Loops/desafio1.js
@@ -1,3 +1,19 @@
 // 1. Verificação de Números Pares ou Ímpares
 // Escreva um programa que recebe um número e imprime 'Par' se for par e 'Ímpar' se for ímpar.
 
+function par(número) {
+  if (número % 2 === 0){
+    return 'Par'
+  
+  }else{
+    return 'Impar'
+  }
+  
+}
+
+// Testes
+console.log(par(2) === 'Par' ? "PASSOU" : "FALHOU");      // Teste para número par
+console.log(par(3) === 'Impar' ? "PASSOU" : "FALHOU");    // Teste para número ímpar
+console.log(par(0) === 'Par' ? "PASSOU" : "FALHOU");      // Teste para zero (par)
+console.log(par(-4) === 'Par' ? "PASSOU" : "FALHOU");     // Teste para número negativo par
+console.log(par(-5) === 'Impar' ? "PASSOU" : "FALHOU");   // Teste para número negativo ímpar
